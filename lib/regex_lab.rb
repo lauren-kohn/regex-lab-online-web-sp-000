6be3d6dc777ea1ddee087d6016caca18fe77f929
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-06-24 14:28:24 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/^[A-Z].*[[:punct:]]/)
+  if text.match(/^[A-Z].*[[:punct:]]/)
+    true
 end
 
 def valid_phone_number?(phone)
