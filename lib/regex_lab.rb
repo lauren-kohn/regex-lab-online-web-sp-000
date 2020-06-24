715969b7a61fb1551745290fe9e333cc2c3ec5a2
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-06-24 14:39:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,7 +21,7 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/^[A-Z].*[[:punct:]]/)
     true
-  else text.match(/^[A-Z].*\w\z/)
+  else !text.match(/^[A-Z].*\w\z/)
     false
   #else text.match(/^[a-z].*[[:punct:]]/)
     #false
