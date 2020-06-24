725479bc04227f20f602bf20e3dcd0bb765dd830
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-06-24 14:02:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,25 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiou]/i)
+    # searches for word boundary followed by any vowel, /i includes upper- and lowercase
+    true
+  else !word.match(/\b[aeiou]/i)
+    # searches for words that start with anything that's not aeiou
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\S{0,}\wing/)
+  # searches for word boundary followed by any number of non-whitespace characters followed by -ing
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/)
+  # searches for word boundary followed by five letters followed by a word boundary
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.
 end
 
 def valid_phone_number?(phone)
